1aaebdcdd41c9180a34766053080fe69ffabead6
monzhelesov <roman.monzhelesov@bk.ru>
2026-06-04 00:00:26 +0000

    fix: remove registry iam binding

--- terraform/main/registry.tf
+++ terraform/main/registry.tf
@@ -2,9 +2,3 @@ resource "yandex_container_registry" "main" {
   name      = "statusboard-registry"
   folder_id = var.folder_id
 }
-
-resource "yandex_container_registry_iam_binding" "puller" {
-  registry_id = yandex_container_registry.main.id
-  role        = "container-registry.images.puller"
-  members     = ["serviceAccount:${yandex_iam_service_account.k8s.id}"]
-}
